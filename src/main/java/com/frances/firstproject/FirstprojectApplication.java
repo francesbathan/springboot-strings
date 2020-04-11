a71c1e5d279dc1dd3af053a64185d76c79a92e1e
frances <frncsbthn@gmail.com>
2020-04-11 13:04:15 -0700

    init commit

---
+++ src/main/java/com/frances/firstproject/FirstprojectApplication.java
@@ -0,0 +1,31 @@
+package com.frances.firstproject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 2. Importing dependencies
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+
+// 1. Annotation
+@RestController
+public class FirstprojectApplication {
+        public static void main(String[] args) {
+                SpringApplication.run(FirstprojectApplication.class, args);
+        }
+        
+        // 1. Annotation
+        @RequestMapping("/")
+
+        // 3. Method that maps to the request route above
+        public String hello() { // 3
+                return "Hello client! How are you doing?";
+        }
+        
+        @RequestMapping("/random")
+
+        // 3. Method that maps to the request route above
+        public String random() { // 3
+                return "Spring Boot is great! So easy to just respond with strings";
+        }
+}
